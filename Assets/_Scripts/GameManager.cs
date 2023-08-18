e0d4de656084533f7de619dc97b0fb5d95ead79e
Leon Matsuki <lmatsuki87+gdvlm@gmail.com>
2023-08-17 22:59:57 -0700

    Hook up start button

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject startMenuCanvas;
+    [SerializeField] private GameObject playCanvas;
+
+    public void StartGame()
     {
-        
+        startMenuCanvas.SetActive(false);
+        playCanvas.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ReturnToStartMenu()
     {
-        
+        playCanvas.SetActive(false);
+        startMenuCanvas.SetActive(true);
     }
 }
