07d8641095a97bf3ef4794d532b7e8754dcb387f
Leon Matsuki <lmatsuki87+gdvlm@gmail.com>
2023-08-25 19:41:17 -0700

    Implement Defeat menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,10 +4,12 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject startMenuCanvas;
     [SerializeField] private GameObject guiCanvas;
+    [SerializeField] private GameObject defeatCanvas;
 
     void Start()
     {
         startMenuCanvas.SetActive(true);
+        defeatCanvas.SetActive(false);
     }
 
     public void StartGame()
@@ -18,6 +20,7 @@ public class GameManager : MonoBehaviour
 
     public void ReturnToStartMenu()
     {
+        defeatCanvas.SetActive(false);
         startMenuCanvas.SetActive(true);
         guiCanvas.SetActive(false);
     }
