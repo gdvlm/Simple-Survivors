07188ede54378669ad015ef6baaf6ac213d9a143
Leon Matsuki <lmatsuki87+gdvlm@gmail.com>
2023-08-25 19:33:21 -0700

    Show start menu by default

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,19 +4,21 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject startMenuCanvas;
     [SerializeField] private GameObject guiCanvas;
-    [SerializeField] private GameObject playCanvas;
+
+    void Start()
+    {
+        startMenuCanvas.SetActive(true);
+    }
 
     public void StartGame()
     {
         startMenuCanvas.SetActive(false);
         guiCanvas.SetActive(true);
-        playCanvas.SetActive(true);
     }
 
     public void ReturnToStartMenu()
     {
         startMenuCanvas.SetActive(true);
         guiCanvas.SetActive(false);
-        playCanvas.SetActive(false);
     }
 }
